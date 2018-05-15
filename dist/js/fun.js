a54dc211dc2287781402785cb3e63d5b82854346
huang <2861629266@qq.com>
2018-05-15 12:17:15 +0800

    5.15中午提交

---
+++ dist/js/fun.js
@@ -0,0 +1,1 @@
+$(function(){$(".tw_ma_right").on("mouseenter",function(){$(".max_ma").css("display","block"),$(".max_ma").css("z-index",111)}),$(".max_ma").on("mouseleave",function(){$(".max_ma").css("display","none"),$(".max_ma").css("z-index",-111)}),$(".distribution_box").on("click",function(){console.log(123),$(this).css("border","1px solid black")})});
